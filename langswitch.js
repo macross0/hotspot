64422a17e904564364472a3e5823125ae6cf10af
macross0 <maxnekuga@gmail.com>
2022-05-25 11:53:07 +0300

    Design update 6

--- langswitch.js
+++ langswitch.js
@@ -1,37 +1,59 @@
-let isUKR = true;
+let buttonUKR, buttonENG;
+let oppositeLanguage;
 
-function disableTransitionOnLoad() {
-    let buttonUKR = document.getElementById("ukr");
-    let buttonENG = document.getElementById("eng");
-    buttonUKR.style.transition = "none !important";
-    buttonENG.style.transition = "none !important";
-    console.log('yes');
+// Run auto language switch at page load
+window.onload = startup;
+
+function startup() {
+    var userLang = navigator.language || navigator.userLanguage;
+    if (userLang == "uk" || "uk-UA") {
+        oppositeLanguage = "eng";
+
+        buttonUKR = document.getElementById("ukr");
+        buttonUKR.style.transitionTimingFunction = "steps(1, jump-start)";
+        updateSwitcher(buttonUKR, true);
+
+        setTimeout(function () {
+            buttonUKR.style.transitionTimingFunction = "ease-in-out";
+        }, 100)
+    }
+    else {
+        oppositeLanguage = "ukr";
+        
+        buttonENG = document.getElementById("eng");
+        buttonENG.style.transitionTimingFunction = "steps(1, jump-start)";
+        updateSwitcher(buttonENG, true);
+
+        setTimeout(function () {
+            buttonENG.style.transitionTimingFunction = "ease-in-out";
+        }, 100)
+    }
 }
 
-function switchLanguage() {
-
-    let buttonUKR = document.getElementById("ukr");
-    let buttonENG = document.getElementById("eng");
+function updateSwitcher(self, startup = false) {
+    if (self.id == oppositeLanguage && !startup) {
+        return;
+    }
+    oppositeLanguage = (oppositeLanguage == "ukr" ? "eng" : "ukr");
 
     let background = document.getElementById("lang-background");
 
-    if (isUKR) {
-        buttonUKR.style.backgroundPosition = "0 0";
+    if (self.id == "ukr") {
+        let buttonENG = document.getElementById("eng");
+
+        self.style.backgroundPosition = "0 0";
         buttonENG.style.backgroundPosition = "100% 0";
 
         background.style.transform = "translate(0, 0)";
 	    background.style.borderRadius = "3px 0 0 3px";
     }
-    else {
+    else{
+        let buttonUKR = document.getElementById("ukr");
         
         buttonUKR.style.backgroundPosition = "-100% 0";
-        buttonENG.style.backgroundPosition = "0 0";
+        self.style.backgroundPosition = "0 0";
 
         background.style.transform = "translate(100%, 0)";
 	    background.style.borderRadius = "0 3px 3px 0";
     }
-
-    isUKR = !isUKR
 }
-
-window.onload = switchLanguage;
